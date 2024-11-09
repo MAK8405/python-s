cf0cc790c980d20e0c69cf7dd8bd86b3614d26ca
Mahdi Ahmadi <mahdiahmadi2005kf@gmail.com>
2024-11-09 12:30:45 +0330

    Add Weight.py

---
+++ Weight.py
@@ -0,0 +1,9 @@
+# the input() function always returns a string by default, no matter what the user types in.
+weight = float(input("Weight: "))
+unit = input("(K)g or (L)bs: ")
+if unit.upper() == "K":
+    converted = weight / 0.45
+    print("Weight in Lbs: " + str(converted))
+else:
+    converted = weight * 0.45
+    print("Weight in Kgs: " + str(converted))
