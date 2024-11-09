1d802bdb3d35f49a38b969eff5dc28ecbe5be1b9
Mahdi Ahmadi <mahdiahmadi2005kf@gmail.com>
2024-11-09 18:32:55 +0330

    Add Range.py

---
+++ Range.py
@@ -0,0 +1,23 @@
+numbers = range(5)
+print(numbers)
+print("")
+for number in numbers:
+    print(number)
+
+print("")
+
+numbers = range(5, 10)
+print(numbers)
+print("")
+for number in numbers:
+    print(number)
+
+print("")
+
+print(numbers)
+print("")
+for number in numbers:
+    print(number)
+print("")
+for number in range(5):
+    print(number)
