583980dbbfc10efa2dece561ded376a87aca82ad
Mahdi Ahmadi <mahdiahmadi2005kf@gmail.com>
2024-11-09 12:25:42 +0330

    Add IfTemperature.py

---
+++ IfTemperature.py
@@ -0,0 +1,19 @@
+temperature = 25
+
+if temperature > 30:
+    print("It's a hot day")
+    print("Drint plenty of water")
+
+elif temperature > 20:
+    print("It's a nice day")
+
+elif temperature > 10:
+    print("It's a bit cold")
+
+else:
+    print("It's cold")
+
+if temperature > 40:
+    print("Good")
+
+print("Done")
