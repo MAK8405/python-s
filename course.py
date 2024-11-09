1d9738ff4f876294400f14295ad6c0b38f6b2a7f
Mahdi Ahmadi <mahdiahmadi2005kf@gmail.com>
2024-11-09 08:47:10 +0330

    Add course.py

---
+++ course.py
@@ -0,0 +1,8 @@
+course = "Python is Good"
+print(course.upper())
+print(course.lower())
+print(course.find('y'))
+print(course.find('Good'))
+print(course.replace('Good', 'Great'))
+print("Python" in course)
+print(course)
