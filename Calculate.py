7cf8136adfc3c5dbe07e486bbcccacc7cb816f1f
Mahdi Ahmadi <mahdiahmadi2005kf@gmail.com>
2024-11-09 07:10:05 +0330

    Add Calculate.py

---
+++ Calculate.py
@@ -0,0 +1,9 @@
+first1 = input("Enter the first number: ")
+second1 = input("Enter the second nubmer: ")
+sum1 = float(first1) + float(second1)
+print("Sum:", sum1)
+
+first2 = float(input("Enter the first number: "))
+second2 = float(input("Enter the second number: "))
+sum2 = first2 + second2
+print("Sum: " + str(sum2))
