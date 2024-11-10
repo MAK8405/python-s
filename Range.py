ac2ee7d15ee1c2ffab8b59c8f2c24b44d35f8c61
Mahdi Ahmadi <mahdiahmadi2005kf@gmail.com>
2024-11-10 18:38:36 +0330

    Fix Range.py

--- Range.py
+++ Range.py
@@ -1,6 +1,8 @@
 numbers = range(5)
 print(numbers)
+
 print("")
+
 for number in numbers:
     print(number)
 
@@ -8,16 +10,13 @@ print("")
 
 numbers = range(5, 10)
 print(numbers)
+
 print("")
+
 for number in numbers:
     print(number)
 
 print("")
 
-print(numbers)
-print("")
-for number in numbers:
-    print(number)
-print("")
 for number in range(5):
     print(number)
